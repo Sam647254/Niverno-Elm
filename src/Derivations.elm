424c6a25b4c3c9e23ea6212d45f1f48c40d9a825
Sam Wang <samofcorinth@gmx.ca>
2020-03-03 23:17:17 -0800

    Aldonis plurajn derivaĵojn

--- src/Derivations.elm
+++ src/Derivations.elm
@@ -1,19 +1,33 @@
 module Derivations exposing (..)
 
+-- Words in prose may either be prototype + inflection or
+-- prototype + derivation + inflection
 
-type
-    Derivation
-    -- Noun derivations
+type Inflection
+    -- Noun infletions
     = Nonspecific -- "an apple"
     | Specific -- "a certain apple"
     | Anaphor -- "the (previously mentioned) apple"
     | Archetype -- "apples" as in "I like apples"
     | Associated -- "my apple(s)"
-      -- Verb derivations
-    | Imperative -- "eat!"
+    -- Verb inflections
+    | StartImperative -- "eat!"
+    | StopImperative -- "stop eating!"
     | Started -- not available for instantaneous verbs
     | Finished
     | NotStarted
     | Paused -- not available for integral and instantaneous verbs
     | Stopped -- not available for integral and instantaneous verbs
     | Cancelled -- not available for integral and instantaneous verbs
+    -- Relationship inflections
+    | Right -- "my child"
+    | Left -- "my mother" (not available for bidirectional relationships)
+
+type Derivation
+    -- Verb derivations
+    = Receiver -- noun
+    | Argument1 -- noun
+    | Argument2 -- noun
+    -- Relationship derivations
+    | Forward -- "is mother of" or "is friend of" -- verb
+    | Reverse -- "is child of" (not available for bidirectional relationships) -- verb
